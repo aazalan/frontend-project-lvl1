5836f995fc20ef796323c4f2f2e03c297b4e531c
Alina Abdul <alinka140901@gmail.com>
2022-07-05 21:13:11 +0300

    linking brain-gcd

--- src/games.js
+++ src/games.js
@@ -41,3 +41,25 @@ export const calcGame = () => {
     }
     congratulation(counter, name);
 }
+
+export const gcdGame = () => {
+    const name = greetingMesssage();
+    console.log('Find the greatest common divisor of given numbers.');
+    let counter = 0;
+    let isItCorrectAnswer = true;
+
+    while (counter < 3 && isItCorrectAnswer == true) {
+        const firstNumber = randomNumber();
+        const secondNumber = randomNumber();
+        const expression = `${firstNumber} ${secondNumber}`;
+        let greatestDivisor = 1;
+        for (let i = 0; i <= Math.min(firstNumber, secondNumber); i += 1) {
+            if (firstNumber % i === 0 && secondNumber % i === 0) {
+                greatestDivisor = Math.max(greatestDivisor, i);
+            }
+        }
+        isItCorrectAnswer = gamePlay(expression, String(greatestDivisor), name);
+        counter += 1;
+    }
+    congratulation(counter, name);
+}
